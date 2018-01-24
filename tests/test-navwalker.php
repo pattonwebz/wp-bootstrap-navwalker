ce21186ed29a5c6fb356f16e1a88e1aeaeb6b2c9
William Patton <will@pattonwebz.com>
2018-01-24 20:18:32 +0000

    Add a test for fallback to check it outputs and return and echo matches

--- tests/test-navwalker.php
+++ tests/test-navwalker.php
@@ -42,7 +42,7 @@ class Test_WP_Bootstrap_NavWalker extends WP_UnitTestCase {
 	 * @access public
 	 * @return void
 	 */
-	function test_startlvl_function() {
+	function test_startlvl_function_exists() {
 
 		$wp_bootstrap_navwalker = $this->walker;
 
@@ -59,7 +59,7 @@ class Test_WP_Bootstrap_NavWalker extends WP_UnitTestCase {
 	 * @access public
 	 * @return void
 	 */
-	function test_start_el_function() {
+	function test_start_el_function_exists() {
 
 		$wp_bootstrap_navwalker = $this->walker;
 
@@ -76,7 +76,7 @@ class Test_WP_Bootstrap_NavWalker extends WP_UnitTestCase {
 	 * @access public
 	 * @return void
 	 */
-	function test_display_element_function() {
+	function test_display_element_function_exists() {
 
 		$wp_bootstrap_navwalker = $this->walker;
 
@@ -93,7 +93,7 @@ class Test_WP_Bootstrap_NavWalker extends WP_UnitTestCase {
 	 * @access public
 	 * @return void
 	 */
-	function test_fallback_function() {
+	function test_fallback_function_exists() {
 
 		$wp_bootstrap_navwalker = $this->walker;
 
@@ -102,5 +102,30 @@ class Test_WP_Bootstrap_NavWalker extends WP_UnitTestCase {
 			'Class does not have method fallback.'
 		);
 
+	}
+
+	function test_fallback_function_outputs() {
+
+		// sample args.
+		$args = array(
+			'container'       => 'div',
+			'container_id'    => 'a_container_id',
+			'container_class' => 'a_container_class',
+			'menu_class'      => 'a_menu_class',
+			'menu_id'         => 'a_menu_id',
+		);
+
+		// default is to echo reults, buffer.
+		ob_start();
+		WP_Bootstrap_Navwalker::fallback( $args );
+		$fallback_output_echo = ob_get_clean();
+		$this->assertNotEmpty( $fallback_output_echo );
+
+		// set 'echo' to false and request the markup returned.
+		$args['echo'] = false;
+		$fallback_output_return = WP_Bootstrap_Navwalker::fallback( $args );
+		$this->assertNotEmpty( $fallback_output_return );
+		// return and echo should result in the same values.
+		$this->assertEquals( $fallback_output_echo, $fallback_output_return );
 	}
 }
